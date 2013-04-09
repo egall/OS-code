760a0981c6af90190e804cc73c0b006e0fe931ac
Erik Quenon Steggall <egall@Bmore.(none)>
2013-04-08 18:14:58 -0700

    Added parse_command() and sys_exit() to shell

--- TA/myshell.c
+++ TA/myshell.c
@@ -3,16 +3,44 @@
 #include <sys/types.h>
 #include <errno.h>
 
-extern char **getline();
+extern char **get_line();
 
-int main() {
+/* Function exits shell when called 
+ * args: none
+ * return: none 
+ *                                */
+void sys_exit(void){
+    exit(EXIT_SUCCESS);
+}
+
+/* Function parses the string given
+ * from the command prompt
+ * args: characters entered 
+ * returns: none 
+ *                                */
+
+void parse_command(char **args){
+
+}
+/* main(): takes input from keyboard
+ * and passes it to parse_command
+ *                                  */
+
+main() {
   int i;
   char **args; 
 
   while(1) {
-    args = getline();
+    /* Print my prompt */
+    printf("egall@baltimore>");
+    args = get_line();
+    /* Don't hang on return */
+    if(args[0] == 0) continue;
+    /* Cycle through args */
     for(i = 0; args[i] != NULL; i++) {
+      /* print out current arg */
       printf("Argument %d: %s\n", i, args[i]);
     }
+    parse_command(args);
   }
 }
